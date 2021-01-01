bfa5a33aa8b51ad54186f7aa3e2cce35e844783a
Kzoeps <kzoepa@gmail.com>
2021-01-01 19:24:58 +0600

    feat(about-me): about me has been migrated, made the collaborations into components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/home";
 import {BrowserRouter, Switch, Route } from "react-router-dom";
 import Beats from "./pages/beats/beats";
 import CartReview from "./pages/cart-review/cart-review";
+import AboutMe from "./pages/about-me/about-me";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 			<Route exact path={'/'} component={Home}/>
 			<Route exact path={'/beats'} component={Beats}/>
 			<Route exact path={'/cart-review'} component={CartReview}/>
+			<Route exact path={'/about-me'} component={AboutMe}/>
 		</Switch>
 	</BrowserRouter>
   );
