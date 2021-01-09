a16c7153f1da0f1edeca24777152da0c16bee324
Kzoeps <kzoepa@gmail.com>
2021-01-09 11:38:00 +0600

    feat(header): header fix, margin top to padding top

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,7 @@ import styles from './header.module.css';
 import utilStyles from '../../assets/global-styles/utils.module.css';
 export default function Header(props) {
 	return (
-		<div className={`${utilStyles.headers} ${styles.headers}`}>
+		<div className={`${styles.headers}`}>
 			<h3 className={`${utilStyles.title}`}>{props.title}</h3>
 			<hr className={utilStyles.hr}/>
 		</div>
