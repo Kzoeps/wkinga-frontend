ee2109c5f6190846bb779165fd30fbe4954bd578
Kzoeps <kzoepa@gmail.com>
2021-01-12 21:56:47 +0600

    feat(contact): contact form validation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import AboutMe from "./pages/about-me/about-me";
-import Contact from './pages/contact/contact'
 import BeatsContainer from "./pages/beats/beats-container";
 import HomeContainer from "./pages/home/home-container";
 import CartReviewContainer from "./pages/cart-review/cart-review-container";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CheckoutContainer from "./pages/checkout/checkout-container";
+import ContactContainer from "./pages/contact/contact-container";
 
 function App() {
 	return (
@@ -17,7 +17,7 @@ function App() {
 				<Route exact path={'/beats'} component={BeatsContainer}/>
 				<Route exact path={'/cart-review'} component={CartReviewContainer}/>
 				<Route exact path={'/about-me'} component={AboutMe}/>
-				<Route exact path={'/contact'} component={Contact}/>
+				<Route exact path={'/contact'} component={ContactContainer}/>
 				<Route exact path={'/checkout'} component={CheckoutContainer}/>
 			</Switch>
 		</BrowserRouter>
--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,7 +1,7 @@
 import utilStyles from '../../assets/global-styles/utils.module.css';
 export default function Button(props) {
 	return (
-		<div className={utilStyles.button}>
+		<div className={utilStyles.button} onClick={props.onClick}>
 			{props.buttonLabel||"Pass props"}
 		</div>
 	)
